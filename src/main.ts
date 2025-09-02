6afefa56f5d0224e42970fa12aaf7b965cfdb361
Titiphan <titiphan.pong@gmail.com>
2025-09-02 19:08:29 +0700

    fix origin enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // ✅ ระบุ origin ที่อนุญาตแบบชัดเจน (สำคัญมากสำหรับ Cloud Run)
+  app.enableCors({
+    origin: ['https://glossy-design.vercel.app', 'http://localhost:3000'], // ✅ ใส่ domain จริงของ frontend
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  // LOCALHOST
-  // const port = process.env.PORT || 3001;
-  // await app.listen(3001);
-
-  // PRODUCTION
   const port = process.env.PORT || 8080;
   await app.listen(port, '0.0.0.0');
   console.log(`API running on http://localhost:${port}`);
