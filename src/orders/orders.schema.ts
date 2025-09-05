8a414fdfad2587931900ed6c57f123a2de8445e1
Titiphan <titiphan.pong@gmail.com>
2025-09-06 03:27:18 +0700

    FIX schema.ts send DB

--- src/orders/orders.schema.ts
+++ src/orders/orders.schema.ts
@@ -5,32 +5,47 @@ export type OrderDocument = Order & Document;
 
 @Schema({ timestamps: true })
 export class Order {
+  // 👉 Base fields
+
   @Prop({ required: true })
   orderId: string;
 
-  @Prop({
-    type: [
-      {
-        category: String,
-        name: String,
-        variant: String,
-        qty: Number,
-        unitPrice: Number,
-        totalPrice: Number,
-        extra: { type: Object },
-      },
-    ],
-  })
-  cart: any[];
+  @Prop()
+  customerName: string;
+
+  @Prop()
+  companyName: string;
+
+  @Prop()
+  note: string;
+
+  @Prop({ required: true })
+  category: string;
 
   @Prop({ required: true })
   total: number;
+
+  @Prop({ default: 0 })
+  discount: number;
 
   @Prop({ enum: ['cash', 'promptpay'], required: true })
   payment: 'cash' | 'promptpay';
 
   @Prop({ enum: ['pending', 'paid', 'cancelled'], default: 'pending' })
   status: string;
+
+  // 👉 รายการสินค้า (cart)
+  @Prop({
+    type: [
+      {
+        name: String, // ชื่อสินค้า เช่น นามบัตร
+        unitPrice: Number, // ราคาต่อหน่วย
+        totalPrice: Number, // ราคารวม
+        extra: { type: Object }, // รายละเอียดเฉพาะแต่ละ Category
+      },
+    ],
+  })
+  cart: any[];
 }
 
 export const OrderSchema = SchemaFactory.createForClass(Order);
